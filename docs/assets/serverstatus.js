ddff0d931133ddd2359ac3443de6699e5d43b35c
IMZCC <i@imzcc.com>
2024-07-08 15:23:59 +0800

    chang discord -> kook;change symbol to salx

--- docs/assets/serverstatus.js
+++ docs/assets/serverstatus.js
@@ -5,18 +5,18 @@
     if (data.online == true) {
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-green-600");
-        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">Play Now: ${data.players.online} Online</span>`;
+        document.getElementById("serverPlaynow").innerHTML = `<span class="inline-block bg-yellow-500 rounded-full px-3 py-1 text-sm font-thin uppercase text-gray-900 mr-2 shadow-lg">MC启动: ${data.players.online} 在线</span>`;
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} playing</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;Online, ${data.players.online} playing</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;We're online!&ensp;&middot;&ensp;${data.players.online} 位玩家正在游戏中</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-green-600">&#11044;</span>&emsp;运行中, ${data.players.online} 位玩家正在游戏中</p>`;
         {% endif %}
     }
     else {
         {% if site.siteNav %}
         document.getElementById("serverInd").classList.add("text-red-600");
         {% else %}
-        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Sorry! We're offline...</p>`;
-        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;Offline</p>`;
+        document.getElementById("serverStatus").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;抱歉！当前服务器离线中...</p>`;
+        document.getElementById("serverStatusM").innerHTML = `<p class="font-bold select-none"><span class="text-red-600">&#11044;</span>&emsp;离线</p>`;
         {% endif %}
     }
 });
--- docs/index.md
+++ docs/index.md
@@ -4,15 +4,15 @@ navname: Home
 
 # Cells at the bottom of the page
 cellLinks:
-  - cname: Server Map
+  - cname: 服务器地图
     curl: map
     cimage: media/demo02.jpg
 
-  - cname: Screenshots
+  - cname: 服务器图片
     curl: screenshots
     cimage: media/demo03.jpg
 
-  - cname: Donate
-    curl: donate
+  - cname: 叽叽歪歪 - 论坛
+    curl: bbs
     cimage: media/demo04.jpg
 ---
